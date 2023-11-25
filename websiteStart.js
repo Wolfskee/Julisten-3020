8a6e26795bf5b29aadde3c1ef24f041d5e85bdf1
khaledelgohary2002 <elgohark@myumanitoba.ca>
2023-11-25 03:20:48 -0600

    few animations and style edits

--- websiteStart.js
+++ websiteStart.js
@@ -1,8 +1,19 @@
 gsap.from("#logo", {
-    duration: 0.75,      // Duration of the animation
-    scale: 0.5,       // Start scale
-    ease: "bounce.out", // Easing function for the bounce effect
-    opacity: 0,       // Start with an opacity of 0
-    y: -100           // Start 100px above its final position
+    duration: 0.75,    
+    scale: 0.5,     
+    ease: "bounce.out",
+    opacity: 0,       
+    y: -100          
 });
+
+gsap.from("header", {
+    duration: 1.5,      
+    scale: 0.9,         
+    opacity: 0,        
+    ease: "expo.out", 
+    y: 20,       
+    rotationX: 10,    
+    transformOrigin: "center bottom" 
+});
+
 
