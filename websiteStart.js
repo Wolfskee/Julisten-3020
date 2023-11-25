66f8f984d5a6732caddec229fad943110643f867
khaledelgohary2002 <elgohark@myumanitoba.ca>
2023-11-25 02:51:47 -0600

    logo added along with an animation

---
+++ websiteStart.js
@@ -0,0 +1,8 @@
+gsap.from("#logo", {
+    duration: 0.75,      // Duration of the animation
+    scale: 0.5,       // Start scale
+    ease: "bounce.out", // Easing function for the bounce effect
+    opacity: 0,       // Start with an opacity of 0
+    y: -100           // Start 100px above its final position
+});
+
