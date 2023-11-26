7613367476e59e925a16be71050c9c8189fb2dc6
khaledelgohary2002 <elgohark@myumanitoba.ca>
2023-11-26 03:30:30 -0600

    signin validation added, with restrictions.

--- login.js
+++ login.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
     var popup = document.querySelector('.popup');
     var closeBtn = document.querySelector('.close-btn');
     var profileButton = document.getElementById('profileButton');
-  
+    var emailPattern= /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
     function showPopup() {
         $('.overlay').fadeIn(300); // Fade in the overlay
         $('.popup').css('transform', 'translate(-50%, -50%) scale(1)').fadeIn(300);
@@ -16,6 +16,18 @@ document.addEventListener('DOMContentLoaded', (event) => {
             $(this).css('transform', 'translate(-50%, -50%) scale(0.9)');
         });
       }
+
+      function signedIn() {
+        if (emailPattern.test($('#email').val()) && $('#password').val().length > 1) {
+          // Hide the sign-in popup
+          hidePopup();
+          $('#profile-pic').toggle();
+          $('#goose').show();     
+          $('#profileButton').prop('disabled', true);     
+        } else {
+          alert('Please check your credentials!');
+        }
+      }
       
   
       $(document).ready(function() {
@@ -26,9 +38,15 @@ document.addEventListener('DOMContentLoaded', (event) => {
         $('.close-btn').click(function() {
           hidePopup();
         });
+
+        $('.popup .form .form-element button').click(function() {
+          signedIn();
+        })
       });
       
   });
+
+
 
   
 
