3c7c7bd64f88e31cb743e9f767bb2282bda04aa8
khaledelgohary2002 <elgohark@myumanitoba.ca>
2023-11-26 18:29:33 -0600

    animation for dropping media

--- websiteStart.js
+++ websiteStart.js
@@ -17,6 +17,27 @@ gsap.from("header", {
 });
 
 
+var tl = gsap.timeline({ repeat: -1, yoyo: true });
+
+tl.to("#droppable", {
+    duration: 4,
+    y: "-20px",
+    scale: 0.9,
+    rotationX: 10,
+    rotationY: 5,
+    ease: "power1.inOut"
+});
+
+tl.to("#droppable", {
+    duration: 4,
+    y: "0px",
+    scale: 1,
+    rotationX: 0,
+    rotationY: 0,
+    ease: "power1.inOut"
+});
+
+
 const links = document.querySelector('nav').children;
 function handleLinkClick(event) {
 
