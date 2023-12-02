6107688ea111f8ea5ab0ff9e4d0d3cf987c2c8c0
hien.nguyen <hienmuondo@gmail.com>
2023-12-02 17:10:00 -0600

    Fix "view more" still overlays after click an info page

--- src/script/moreInfo.js
+++ src/script/moreInfo.js
@@ -1,26 +1,26 @@
 var information = {
-  "description": [
-    {"Us and Them": "Us and Them is a song by the English progressive rock band Pink Floyd, from their 1973 album The Dark Side of the Moon. The music was written by Richard Wright with lyrics by Roger Waters. It is sung by David Gilmour, with harmonies by Wright. The song is 7 minutes and 49 seconds, the longest on the album. Us and Them was released as the second single from The Dark Side of the Moon in the United States, peaking at No. 72 on the Cash Box Top 100 Singles chart in March 1974. The single peaked at No. 85 in the Canadian chart."},
-    {"11": "11 is a song by the Chinese singer GEM, In 2015, G.E.M. released her first full Mandopop album Heartbeat. The following year, she became the only Asian artist featured in the Forbes 30 Under 30. She is the first female Chinese singer to have four music videos that exceed 100 million views on YouTube."}
-  ]
+	"description": [
+		{ "Us and Them": "Us and Them is a song by the English progressive rock band Pink Floyd, from their 1973 album The Dark Side of the Moon. The music was written by Richard Wright with lyrics by Roger Waters. It is sung by David Gilmour, with harmonies by Wright. The song is 7 minutes and 49 seconds, the longest on the album. Us and Them was released as the second single from The Dark Side of the Moon in the United States, peaking at No. 72 on the Cash Box Top 100 Singles chart in March 1974. The single peaked at No. 85 in the Canadian chart." },
+		{ "11": "11 is a song by the Chinese singer GEM, In 2015, G.E.M. released her first full Mandopop album Heartbeat. The following year, she became the only Asian artist featured in the Forbes 30 Under 30. She is the first female Chinese singer to have four music videos that exceed 100 million views on YouTube." }
+	]
 };
 
 let homePageHTML = null;
 
 
-function moreInfo_SoD(element){
+function moreInfo_SoD(element) {
 	var singer = element.parentElement.querySelectorAll('div')[0].outerHTML;
 	var length = element.parentElement.querySelectorAll('div')[1].outerHTML;
 	var coverPic = element.parentElement.parentElement.querySelector('img').outerHTML;
 	var musicName = element.parentElement.querySelector('h5').textContent;
-  var mainPage = document.getElementById("main-container");
-  homePage = document.getElementById("main-container");
+	var mainPage = document.getElementById("main-container");
+	homePage = document.getElementById("main-container");
 	var info
 	console.log(homePage.children[0]);
 
-  if (!homePageHTML) {
-    homePageHTML = mainPage.innerHTML;
-  }
+	if (!homePageHTML) {
+		homePageHTML = mainPage.innerHTML;
+	}
 
 
 	info = searchForKey(information, musicName);
@@ -32,11 +32,11 @@ function moreInfo_SoD(element){
 			<button class="btn btn-secondary search-button backbutton" onclick="returnHome()"><h2>&larr;</h2></button>
 			<section class="song-of-the-day mb-5">
 				<div class="row">
-					`+coverPic+`
+					`+ coverPic + `
 					<div class="col">
-						<h5>`+musicName+`</h5>
-						`+singer+`
-						`+length+`
+						<h5>`+ musicName + `</h5>
+						`+ singer + `
+						`+ length + `
 					</div>
 				</div>
 			</section>
@@ -44,45 +44,43 @@ function moreInfo_SoD(element){
 			<div class="container-fluid" length: 500px>
 				<h4>Information</h4>
 					<div class="col">
-						`+info+`
+						`+ info + `
 					</div>
 			</div>
 		</div>
 	`;
 }
 
-function moreInfo(element){
+function moreInfo(element) {
+	hideGenre();
 	const musicName = element.parentElement.querySelectorAll('span')[0].outerHTML;
-  const singer = element.parentElement.querySelectorAll('span')[1].outerHTML;
-	//var length = element.parentElement.querySelectorAll('div')[1].outerHTML;
-  console.log(musicName);
-  var lengthMin = (Math.floor(Math.random()*2) + 3).toString();
-  var lengthSec = Math.ceil(Math.random() * 20);
-  if(lengthSec<10){
-    lengthSec = "0" + lengthSec.toString();
-  }
-  var length = lengthMin + ':'+lengthSec;
+	const singer = element.parentElement.querySelectorAll('span')[1].outerHTML;
+	console.log(musicName);
+	var lengthMin = (Math.floor(Math.random() * 2) + 3).toString();
+	var lengthSec = Math.ceil(Math.random() * 20);
+	if (lengthSec < 10) {
+		lengthSec = "0" + lengthSec.toString();
+	}
+	var length = lengthMin + ':' + lengthSec;
 	var coverPic = document.createElement("img")
-  coverPic.src = element.parentElement.parentElement.querySelector('img').src
-  coverPic.alt = element.parentElement.parentElement.querySelector('img').alt;
-  coverPic.className = "mr-3 album-art";
-  coverPic.draggable = false;
-  coverPic = coverPic.outerHTML;
-  var mainPage = document.getElementById("main-container");
+	coverPic.src = element.parentElement.parentElement.querySelector('img').src
+	coverPic.alt = element.parentElement.parentElement.querySelector('img').alt;
+	coverPic.className = "mr-3 album-art";
+	coverPic.draggable = false;
+	coverPic = coverPic.outerHTML;
+	var mainPage = document.getElementById("main-container");
 	var info;
 
 
+	homePageHTML = mainPage.innerHTML;
 
-
-  homePageHTML = mainPage.innerHTML;
-
-  //console.log(homePageHTML);
+	//console.log(homePageHTML);
 
 	info = searchForKey(information, musicName);
 
-  if(info == undefined){
-    info = "This is a general description of "+musicName +"is a good music"
-  }
+	if (info == undefined) {
+		info = "This is a general description of " + musicName + "is a good music"
+	}
 
 	console.log(info);
 
@@ -91,11 +89,11 @@ function moreInfo(element){
 			<button class="btn btn-secondary search-button backbutton" onclick="returnHome()"><h2>&larr;</h2></button>
 			<section class="song-of-the-day mb-5">
 				<div class="row">
-					`+coverPic+`
+					`+ coverPic + `
 					<div class="col">
-						<h5>`+musicName+`</h5>
-						`+singer+`
-						<div>`+length+`</div>
+						<h5>`+ musicName + `</h5>
+						`+ singer + `
+						<div>`+ length + `</div>
 					</div>
 				</div>
 			</section>
@@ -103,32 +101,32 @@ function moreInfo(element){
 			<div class="container-fluid" length: 500px>
 				<h4>Information</h4>
 					<div class="col">
-						`+info+`
+						`+ info + `
 					</div>
 			</div>
 		</div>
 	`;
 }
 
-function moreInfo_callFromQueue(element){
+function moreInfo_callFromQueue(element) {
 	var musicName = element.previousSibling.textContent.split("Artist: ")[0];
-  var singer = element.previousSibling.textContent.split("Artist: ")[1];
+	var singer = element.previousSibling.textContent.split("Artist: ")[1];
 	var coverPic = element.previousSibling.querySelector('img').outerHTML;
-  var mainPage = document.getElementById("main-container");
-  var info;
+	var mainPage = document.getElementById("main-container");
+	var info;
 
-  //homePageHTML = mainPage.innerHTML;
-  if (!homePageHTML) {
-    homePageHTML = mainPage.innerHTML;
-  }
+	//homePageHTML = mainPage.innerHTML;
+	if (!homePageHTML) {
+		homePageHTML = mainPage.innerHTML;
+	}
 
-  // console.log(homePageHTML);
-  //
+	// console.log(homePageHTML);
+	//
 	info = searchForKey(information, musicName);
-  //
-  if(info == undefined){
-    info = "This is a general description of "+musicName +"is a good music"
-  }
+	//
+	if (info == undefined) {
+		info = "This is a general description of " + musicName + "is a good music"
+	}
 
 	console.log(info);
 
@@ -137,11 +135,11 @@ function moreInfo_callFromQueue(element){
 	 		<button class="btn btn-secondary search-button backbutton" onclick="returnHome()"><h2>&larr;</h2></button>
 	 		<section class="song-of-the-day mb-5">
 	 			<div class="row">
-	 				`+coverPic+`
+	 				`+ coverPic + `
 	 				<div class="col">
-	 					<h5>`+musicName+`</h5>
-	 					Artist: `+singer+`
-	 					<div>`+length+`</div>
+	 					<h5>`+ musicName + `</h5>
+	 					Artist: `+ singer + `
+	 					<div>`+ length + `</div>
 	 				</div>
 	 			</div>
 	 		</section>
@@ -149,29 +147,29 @@ function moreInfo_callFromQueue(element){
 	 		<div class="container-fluid" length: 500px>
 	 			<h4>Information</h4>
 	 				<div class="col">
-	 					`+info+`
+	 					`+ info + `
 	 				</div>
 	 		</div>
 	 	</div>
 	 `;
 }
 
-function returnHome(){
-  var mainPage = document.getElementById("main-container");
+function returnHome() {
+	var mainPage = document.getElementById("main-container");
 
-  // Set the innerHTML of the main page back to the original home page HTML
-  console.log(homePageHTML);
-  if (homePageHTML) {
-    mainPage.innerHTML = homePageHTML;
-  }
+	// Set the innerHTML of the main page back to the original home page HTML
+	console.log(homePageHTML);
+	if (homePageHTML) {
+		mainPage.innerHTML = homePageHTML;
+	}
 }
 
 function searchForKey(info, keyToSearch) {
-  var result;
-  info.description.forEach(function(item) {
-    if(item[keyToSearch]) {
-      result = item[keyToSearch];
-    }
-  });
-  return result;
+	var result;
+	info.description.forEach(function (item) {
+		if (item[keyToSearch]) {
+			result = item[keyToSearch];
+		}
+	});
+	return result;
 }
